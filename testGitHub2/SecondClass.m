a2da8d117f195e66c772f6f777b9192a83e953b8
kwansoo <kwansoo.ahn@rgpkorea.co.kr>
2016-02-19 11:31:10 +0900

    make second class

---
+++ testGitHub2/SecondClass.m
@@ -0,0 +1,13 @@
+//
+//  SecondClass.m
+//  testGitHub2
+//
+//  Created by ahnkwansoo on 2/19/16.
+//  Copyright © 2016 ahnkwansoo. All rights reserved.
+//
+
+#import "SecondClass.h"
+
+@implementation SecondClass
+
+@end
