1d580626dd405d3a420268d6eeca082e002294bd
kwansoo <kwansoo.ahn@rgpkorea.co.kr>
2016-02-19 11:26:40 +0900

    make first class

---
+++ testGitHub2/FirstClass.h
@@ -0,0 +1,13 @@
+//
+//  FirstClass.h
+//  testGitHub2
+//
+//  Created by ahnkwansoo on 2/19/16.
+//  Copyright © 2016 ahnkwansoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstClass : NSObject
+
+@end
---
+++ testGitHub2/FirstClass.m
@@ -0,0 +1,13 @@
+//
+//  FirstClass.m
+//  testGitHub2
+//
+//  Created by ahnkwansoo on 2/19/16.
+//  Copyright © 2016 ahnkwansoo. All rights reserved.
+//
+
+#import "FirstClass.h"
+
+@implementation FirstClass
+
+@end
